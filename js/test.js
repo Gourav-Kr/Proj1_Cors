d13732982427b6104726a7ace17ca71fc9e7bc3d
Gourav Kumar <gouravcoc667@gmail.com>
2021-12-01 12:56:52 +0530

    hopefully final

--- js/test.js
+++ js/test.js
@@ -2,7 +2,7 @@ check = document.getElementById("navi-toggle");
 attri = document.querySelectorAll(".navigation__item")
 console.log(attri)
 for (let index = 0; index < attri.length; index++) {
-    
+
     attri[index].addEventListener("click", function () {
         console.log(check.checked)
         check.checked = false;
@@ -15,19 +15,39 @@ for (let index = 0; index < attri.length; index++) {
 // });
 
 var modal = document.getElementById("popup");
-var btn = document.getElementById("pop_but");
-
+var btn = document.querySelector("#pop_but");
+var btn1 = document.querySelector("#pop_but1");
+var btn2 = document.querySelector("#pop_but2");
+var close = document.querySelector(".popup__close");
+ 
+// for (let index = 0; index < btn.length; index++) {
 btn.addEventListener("click", function () {
     // modal.style.display = "block";
-    modal.style.opacity= 1;
+    modal.style.opacity = 1;
     modal.style.visibility = "visible";
 });
-  
+btn1.addEventListener("click", function () {
+    // modal.style.display = "block";
+    modal.style.opacity = 1;
+    modal.style.visibility = "visible";
+});
+btn2.addEventListener("click", function () {
+    // modal.style.display = "block";
+    modal.style.opacity = 1;
+    modal.style.visibility = "visible";
+});
+// }
 
-window.onclick = function(event) {
+close.addEventListener("click", function () {
+    // modal.style.display = "block";
+    modal.style.opacity = 0;
+    modal.style.visibility = "hidden";
+});
+
+window.onclick = function (event) {
     if (event.target == modal) {
         // modal.style.display = "none";
-        modal.style.opacity= 0;
+        modal.style.opacity = 0;
         modal.style.visibility = "hidden";
     }
-  }
+}
